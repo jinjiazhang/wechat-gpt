7b76b39eb2e48b8dda5ee0d6988227549e66d5e3
jinjiazhang <jinjiazhang@tencent.com>
2023-02-14 16:37:45 +0800

    简单调用chatgpt接口

--- logic.go
+++ logic.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"context"
+	"encoding/xml"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
+
+type MessageReq struct {
+	ToUserName   string `xml:"ToUserName"`
+	FromUserName string `xml:"FromUserName"`
+	CreateTime   int64  `xml:"CreateTime"`
+	MsgType      string `xml:"MsgType"`
+	Content      string `xml:"Content"`
+	MsgId        int64  `xml:"MsgId"`
+	MsgDataId    int64  `xml:"MsgDataId"`
+	Idx          int64  `xml:"Idx"`
+	Event        string `xml:"Event"`
+}
+
+type MessageRsp struct {
+	XMLName      xml.Name `xml:"xml"`
+	ToUserName   string   `xml:"ToUserName"`
+	FromUserName string   `xml:"FromUserName"`
+	CreateTime   int64    `xml:"CreateTime"`
+	MsgType      string   `xml:"MsgType"`
+	Content      string   `xml:"Content"`
+}
 
 func WeChatMessage(ctx context.Context, req *MessageReq) (*MessageRsp, error) {
 	reply, err := RequestChatGPT(req.Content)
 	if err != nil {
+		log.Errorf("WeChatMessage request fail, err: %+v", err)
 		reply = err.Error()
 	}
 
