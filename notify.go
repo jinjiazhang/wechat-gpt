1c698a73e4589d629c6a4b9b628ab6485c85adc2
jinjiazhang <jinjiazhang@tencent.com>
2023-02-24 11:55:11 +0800

    聊天机器人

---
+++ notify.go
@@ -0,0 +1,5 @@
+package main
+
+func NotifyUser(openid string, text string) error {
+	return nil
+}
--- session.go
+++ session.go
@@ -1,16 +1,84 @@
 package main
 
+import (
+	"sync"
+	"time"
+)
+
+var (
+	sessionMap map[string]*Session
+	mapMutex   sync.Mutex
+)
+
+type Message struct {
+	author string
+	text   string
+	time   int64
+}
+
 type Session struct {
+	openid   string
+	messages []*Message
+	mutex    sync.Mutex
+}
+
+func GetSession(openid string) *Session {
+	mapMutex.Lock()
+	defer mapMutex.Unlock()
+
+	if session, ok := sessionMap[openid]; ok {
+		return session
+	}
+
+	session := &Session{
+		openid:   openid,
+		messages: make([]*Message, 0),
+	}
+	sessionMap[openid] = session
+	return session
 }
 
 func (s *Session) Chat(text string) error {
+	s.push(&Message{
+		author: "Human",
+		text:   text,
+		time:   time.Now().Unix(),
+	})
+
+	go func() {
+		reply, err := RequestChatGPT(s.prompt())
+		if err != nil {
+			NotifyUser(s.openid, err.Error())
+			return
+		}
+
+		s.push(&Message{
+			author: "Robot",
+			text:   reply,
+			time:   time.Now().Unix(),
+		})
+		NotifyUser(s.openid, reply)
+	}()
+
 	return nil
 }
 
 func (s *Session) Reset() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.messages = make([]*Message, 0)
 	return nil
 }
 
-func GetSession(openid string) *Session {
+func (s *Session) push(message *Message) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.messages = append(s.messages, message)
 	return nil
+}
+
+func (s *Session) prompt() string {
+	return ""
 }
